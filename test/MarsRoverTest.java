93981eaecf1aaf86c230cce42bfe454fbcdab602
parulsharda <sharmanancy007@gmail.com>
2015-07-31 12:29:43 +0530

    #007| Knows to move left and right from west

--- test/MarsRoverTest.java
+++ test/MarsRoverTest.java
@@ -7,12 +7,18 @@ import static junit.framework.TestCase.assertEquals;
 public class MarsRoverTest {
         MarsRover rover;
 
-        @Test
-        public void positionAndDirectionDoesNotChangeWhenRoverTurnsLeftAndRight() {
-            rover = new MarsRover(1, 2, 'N');
+    @Test
+    public void positionAndDirectionDoesNotChangeWhenRoverTurnsLeftAndRight() {
+        rover = new MarsRover(1, 2, 'N');
 
-            assertEquals(new MarsRover(1, 2, 'N'), rover.navigateTo("LR"));
+        assertEquals(new MarsRover(1, 2, 'N'), rover.navigateTo("LR"));
         }
 
+    @Test
+    public void directionChangesToNorthWhenRoverTurnsRightFromWest() {
+        rover = new MarsRover(0, 0, 'W');
+
+        assertEquals(new MarsRover(0, 0, 'N'), rover.navigateTo("R"));
+    }
 
 }
